c85425f43b0f87a5ec810db9506a911cf70492f9
Lena <perlen93@hotmail.com>
2019-05-30 14:27:29 +0200

    UTAN JÄVLA MINNE

--- main.js
+++ main.js
@@ -12,87 +12,9 @@ footer.insertBefore(counter, all);
 let activeToDo = document.getElementsByClassName("active");
 let completedToDo = document.getElementsByClassName("completed");
 
-let memory = window.localStorage;
-let memoryItems =[];
-
-// hämtar allt i minnet o gör det till en array
-let fromMemoryArray= memory.getItem('toDo');
-// om det finns ngt att splita o dela upp en array på, gör det! 
-if (fromMemoryArray != null){
-    let hej =fromMemoryArray.split(',');
-    if (fromMemoryArray.length>0) {    
-        
-        hej.forEach(function(item){
-
-            //printa ut varje grej i en div    
-            let toDoInput = document.getElementById("toDoInput");
-            let div = document.createElement("div");
-            let newToDo = document.createElement("label");
-            newToDo.setAttribute("class","toDoLabel");
-            let checkbox = document.createElement("input");
-            let span = document.createElement("span");
-            span.textContent = item;
-            let imgCross = document.createElement("img");
-
-            div.setAttribute("class", "active");
-            toDoContainter.appendChild(div);
-            checkbox.setAttribute("type", "checkbox");
-            checkbox.setAttribute("class", "checkbox");
-            newToDo.appendChild(checkbox);
-            div.appendChild(newToDo);
-            newToDo.appendChild(span);      
-            imgCross.setAttribute("src", "xclose.png");
-            div.appendChild(imgCross);
-
-            toDoContainter.style.display = 'inline-block';
-            footer.style.display = 'inline-block';
-            markAll.style.display = 'inline-block';  
-            memoryItems.push(item);
-
-            counter.textContent = itemsleft.length + " items left";
-        })
-    }
-}
 
 form.onsubmit = event => {
-    event.preventDefault();
-
-    // om det inte finns ngt i todon kolla om det finns ngt i minnet:
-    if (toDoContainter.length < 2){
-
-        if (fromMemoryArray != null){
-        if(fromMemoryArray.length > 0){
-        let hej =fromMemoryArray.split(',');
-
-            hej.forEach(function(item) {
-                //printa ut varje grej i en div    
-                let toDoInput = document.getElementById("toDoInput");
-                let div = document.createElement("div");
-                let newToDo = document.createElement("label");
-                newToDo.setAttribute("class","toDoLabel");
-                let checkbox = document.createElement("input");
-                let span = document.createElement("span");
-                span.textContent = item;
-                let imgCross = document.createElement("img");
-    
-                div.setAttribute("class", "active");
-                toDoContainter.appendChild(div);
-                checkbox.setAttribute("type", "checkbox");
-                checkbox.setAttribute("class", "checkbox");
-                newToDo.appendChild(checkbox);
-                div.appendChild(newToDo);
-                newToDo.appendChild(span);      
-                imgCross.setAttribute("src", "xclose.png");
-                div.appendChild(imgCross);
-        
-                toDoContainter.style.display = 'inline-block';
-                footer.style.display = 'inline-block';
-                markAll.style.display = 'inline-block';  
-                memoryItems.push(item);
-            });
-        }
-    }
-    }
+    event.preventDefault();      
 
     let toDoInput = document.getElementById("toDoInput");
     let div = document.createElement("div");
@@ -104,12 +26,6 @@ form.onsubmit = event => {
     let imgCross = document.createElement("img");
 
     if (span.textContent !=""){
-        //lägger till input i en array
-        memoryItems.push(toDoInput.value);
-        // her de i en sträng då localStorage vill ha det så
-        let memoryString = memoryItems.toString();
-        // lägger strängen i minnet
-        memory.setItem('toDo',memoryString)
 
         div.setAttribute("class", "active");
         toDoContainter.appendChild(div);
@@ -251,9 +167,6 @@ completedButton.onclick = event => {
 
 let clearCompleted = document.getElementById("clearCompleted")
 clearCompleted.onclick = event => {
-    memory.clear();
-    fromMemoryArray =[];
-    memoryItems = [];
 
     while (completedToDo.length > 0){
         completedToDo[length].remove();
@@ -279,7 +192,6 @@ clearCompleted.onclick = event => {
 markAll.onclick = event => {
     let toDoContainterArray = Array.from(document.getElementById('create-new-todo'));
 
-    //om någon är checkad ändra alla till active
     if(toDoContainterArray.some(x=> x.checked)){
         var i;
         for (i = 0; i < toDoContainter.length; i++) {
@@ -308,7 +220,6 @@ markAll.onclick = event => {
     else{
         clearCompleted.style.display = "inline-block";
     }
-
 
 }
 
